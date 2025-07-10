86f2ea0a06187ed632b907c0b95fcb3d024fb7e4
edenkry <743840426@qq.com>
2025-07-10 13:03:58 +0800

    Site updated: 2025-07-10 13:03:57

--- js/amusic.js
+++ js/amusic.js
@@ -15,5 +15,12 @@ const ap = new APlayer({
         url: 'http://music.163.com/song/media/outer/url?id=2151230759.mp3',
         cover: 'http://p2.music.126.net/JKOtlxRYpzsqXxC3l07ivA==/109951169547216140.jpg?param=130y130',
    },
+        {
+        name: 'Storyteller',
+        artist: ' Miaa ',
+        url: 'http://music.163.com/song/media/outer/url?id=2148767644.mp3',
+        cover: 'http://p1.music.126.net/_MLhrrMyI3ULlA-zocDmSQ==/109951169525187211.jpg?param=130y130',
+   },
+
 ]
 });
