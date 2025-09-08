2b9cf5312ec3a45367ec2a68f3eff9e4698dd518
edenkry <743840426@qq.com>
2025-09-08 13:14:16 +0800

    Site updated: 2025-09-08 13:14:14

--- js/amusic.js
+++ js/amusic.js
@@ -4,17 +4,19 @@ const ap = new APlayer({
 	  autoplay: false, //关闭自动播放
     audio: [
 	{
+        name: 'Echoism',
+        artist: ' Casey Lee Williams ',
+        url: 'http://music.163.com/song/media/outer/url?id=2151230759.mp3',
+        cover: 'http://p2.music.126.net/JKOtlxRYpzsqXxC3l07ivA==/109951169547216140.jpg?param=130y130',
+    },
+
+        {
         name: 'Succession',
         artist: '北村友香',
         url: 'http://music.163.com/song/media/outer/url?id=2151081433.mp3',
         cover: 'http://p2.music.126.net/gQa7cvwUMrN3AbfpLJQVwQ==/109951169546079916.jpg?param=130y130',
     }, 
-	{
-        name: 'Echoism',
-        artist: ' Casey Lee Williams ',
-        url: 'http://music.163.com/song/media/outer/url?id=2151230759.mp3',
-        cover: 'http://p2.music.126.net/JKOtlxRYpzsqXxC3l07ivA==/109951169547216140.jpg?param=130y130',
-   },
+	
         {
         name: 'Storyteller',
         artist: ' Miaa ',
