936702921ac16379c64fa6585fa2e61a818391c8
edenkry <743840426@qq.com>
2025-09-28 08:55:07 +0800

    Site updated: 2025-09-28 08:55:05

--- js/amusic.js
+++ js/amusic.js
@@ -3,6 +3,13 @@ const ap = new APlayer({
     fixed: true,
 	  autoplay: false, //关闭自动播放
     audio: [
+       {
+        name: 'Symbol III : ▽',
+        artist: ' Ave Mujica ',
+        url: 'http://music.163.com/song/media/outer/url?id=2160763758.mp3',
+        cover: 'http://p2.music.126.net/CBd5ode623rwsqgk1DuGiA==/109951170225580494.jpg?param=130y130',
+    },
+
 	{
         name: 'Echoism',
         artist: ' Casey Lee Williams ',
@@ -10,14 +17,14 @@ const ap = new APlayer({
         cover: 'http://p2.music.126.net/JKOtlxRYpzsqXxC3l07ivA==/109951169547216140.jpg?param=130y130',
     },
 
-     {
+      {
         name: '久遠 ~光と波の記憶~',
         artist: '松枝賀子 ',
         url: 'http://music.163.com/song/media/outer/url?id=622872.mp3',
         cover: 'http://p1.music.126.net/fP7C3sZL8XlVrS59fKVc8g==/891703930126667.jpg?param=130y130',
     },
 
-	   {
+      {
         name: 'Succession',
         artist: '北村友香',
         url: 'http://music.163.com/song/media/outer/url?id=2151081433.mp3',
